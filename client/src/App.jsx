376cd70fffa277c1563df6bc7a034537bd3b9fbd
Nurain <nurainbamidelejnr@gmail.com>
2024-04-21 17:19:06 +0100

    ROutes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+// import Header from "./componenets/Header";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline bg-red-300">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
