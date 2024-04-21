c1172ba7a02b37bca44b9fe0523b7d2a09f3594d
Nurain <nurainbamidelejnr@gmail.com>
2024-04-21 17:23:25 +0100

    Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,12 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-// import Header from "./componenets/Header";
+import Header from "./componenets/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
